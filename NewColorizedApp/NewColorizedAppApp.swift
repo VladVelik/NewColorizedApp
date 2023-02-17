ca9826b8862f00dc28e2224dc5706771a5e9c5c4
VladVelik <sosinvladik@yandex.ru>
2023-02-17 15:41:04 +0300

    Initial Commit

---
+++ NewColorizedApp/NewColorizedAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewColorizedAppApp.swift
+//  NewColorizedApp
+//
+//  Created by Sosin Vladislav on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewColorizedAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
